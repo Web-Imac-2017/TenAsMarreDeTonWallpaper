97c041e195885c2a89343c43e957761871962c2f
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-02-11 10:43:43 +0100

    Implémentation du PHP, transfert des données

--- toast/joris.php
+++ toast/joris.php
@@ -16,31 +16,49 @@
         <h1>joris.php</h1>
         <fieldset>
             <legend>Inscription</legend>
+            <?php 
 
+            if(isset($_POST['submit'])) { // the form has been submitted 
+                if(isset($_POST['pseudo']) && isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['color'])) {
+                    echo "<h3>Bien joué ".$_POST['pseudo'].", t'es inscrit !</h3>";
+                    echo "<ul><li>Mot de passe : ".$_POST['mdp']."</li>";
+                    echo "<li>Adresse mail : ".$_POST['mail']."</li></ul>";
+                    echo "<style>body{color: white; background:".$_POST['color']."</style>";
+                }
+                else
+                    echo "T'as dû faire une erreur !";
+            }
+            else {
+                
+            ?>
             <form action="" method="post">
                 <table>
                     <tr>
                         <td> <label for="pseudo">Votre pseudo :</label></td>
-                        <td><input type="text" id="pseudo" /></td>
+                        <td><input type="text" id="pseudo" name="pseudo" /></td>
                     </tr>
                     <tr>
                         <td><label for="mail">Votre adresse email :</label></td>
-                        <td><input type="mail" id="mail" /></td>
+                        <td><input type="mail" id="mail" name="mail" /></td>
                     </tr>
                     <tr>
                         <td><label for="mdp">Votre mot de passe :</label></td>
-                        <td><input type="password" id="mdp" /></td>
+                        <td><input type="password" id="mdp" name="mdp" /></td>
                     </tr>
                     <tr>
                         <td><label for="color">Votre couleur préférée :</label></td>
-                        <td><input type="color" id="color" /></td>
+                        <td><input type="color" id="color" name="color" /></td>
                     </tr>
                     <tr>
                         <td></td>
-                        <td><input type="submit" value="S'inscire" /></td>
+                        <td><input type="submit" value="S'inscire" name="submit" /></td>
                     </tr>
                 </table>
             </form>
+            <?php
+            }
+
+            ?>
         </fieldset>
     </body>
 </html>
