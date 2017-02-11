3d1286298738382a2d278bc91a3d643c92d119e4
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-02-11 10:27:00 +0100

    Création du fichier joris.php et création d'un formulaire

---
+++ toast/joris.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="">
+
+        <title>Joris</title>
+
+    </head>
+    <body>
+        <h1>joris.php</h1>
+        <fieldset>
+            <legend>Inscription</legend>
+
+            <form action="" method="post">
+                <table>
+                    <tr>
+                        <td> <label for="pseudo">Votre pseudo :</label></td>
+                        <td><input type="text" id="pseudo" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="mail">Votre adresse email :</label></td>
+                        <td><input type="mail" id="mail" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="mdp">Votre mot de passe :</label></td>
+                        <td><input type="password" id="mdp" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="color">Votre couleur préférée :</label></td>
+                        <td><input type="color" id="color" /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="S'inscire" /></td>
+                    </tr>
+                </table>
+            </form>
+        </fieldset>
+    </body>
+</html>
