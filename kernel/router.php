b1e6b175694c0caf35d30b538a059c0ee36ce43f
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-02-24 19:20:41 +0100

    Réorganisation Git

---
+++ kernel/router.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'controllers/ControleurName.php';
+
+$str_json = file_get_contents('php://input');
+$obj = json_decode($str_json);
+
+class Router {
+
+	// A titre d'exemple ; il y aura autant de variables que de controlleurs
+	private $ctrlControllerName;
+
+	public function __construct() {
+	    $this->ctrlControllerName = new ControllerName();
+	}
+
+	public function routerRequete() {
+		foreach($obj as $requeteName) {
+			if ($requeteName == 'name') {
+				// do things ..
+			}
+		}
+	}
+
+}
