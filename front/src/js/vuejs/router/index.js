0592e00507f8515f5968da9810090f5a3ab5ea67
Thaledric <thaledric@gmail.com>
2017-02-27 19:21:44 +0100

    Removed page<x>

--- front/src/js/vuejs/router/index.js
+++ front/src/js/vuejs/router/index.js
@@ -2,30 +2,12 @@
 
 import VueRouter from 'vue-router';
 
-import Page1 from '../components/page1';
-import Page2 from '../components/page2';
-import Page3 from '../components/page3';
 import Home from '../components/pages/home';
 
 const router = new VueRouter({
   mode  : 'history',
   base  : '/TenAsMarreDeTonWallpaper/',
   routes: [
-    {
-      name     : 'page1',
-      path     : '/page1',
-      component: Page1,
-    },
-    {
-      name     : 'page2',
-      path     : '/page2',
-      component: Page2,
-    },
-    {
-      name     : 'page3',
-      path     : '/page3/:item_id',
-      component: Page3,
-    },
     {
       name     : 'home',
       path     : '/',
