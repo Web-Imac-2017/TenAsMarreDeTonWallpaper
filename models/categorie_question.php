11ef3a1842cae1c47955e6fdddd582c1f11ff1e0
Davdouv <d4vid.n@hotmail.fr>
2017-02-28 11:05:39 +0100

    M.A.J. fonctions question / categorie functions

--- models/categorie_question.php
+++ models/categorie_question.php
@@ -6,7 +6,7 @@
 	// Renvoie les informations de toutes les questions
 	function getQuestions() {
 		$bdd = getBdd();
-		$sql = 'SELECT id AS q_id, question_courte AS q_courte, question_longue AS q_longue, importance AS importance, popularite AS popularite FROM questions';
+		$sql = 'SELECT id AS q_id, q_courte AS q_courte, q_longue AS q_longue, importance AS importance, nb_apparition AS nb_apparition FROM question';
 		$questions = $bdd->query($sql);
 		return $questions;
 	}
@@ -14,7 +14,7 @@
 	// Renvoie les informations sur une seule question
 	function getQuestion($questionID) {
 		$bdd = getBdd();
-		$sql = 'SELECT id AS q_id, question_courte AS q_courte, question_longue AS q_longue, importance AS importance, popularite AS popularite FROM questions WHERE id=?';
+		$sql = 'SELECT id AS q_id, q_courte AS q_courte, q_longue AS q_longue, importance AS importance, nb_apparition AS nb_apparition FROM question WHERE id=?';
 		$question = $bdd->prepare($sql);
 		$question->execute(array($questionID));
 		if ($question->rowCount() == 1)
@@ -24,14 +24,14 @@
 	}
 	
 	// Rajoute une question
-	function addQuestion($q_courte, $q_longue, $importance, $popularite, $categories) {
+	function addQuestion($q_courte, $q_longue, $importance, $nb_apparition, $categories) {
 		$bdd = getBdd();
-		$sql = 'INSERT INTO questions(question_courte, question_longue, importance, popularite) VALUES(:q_courte, :q_longue, :importance, :popularite)';
+		$sql = 'INSERT INTO question(q_courte, q_longue, importance, nb_apparition) VALUES(:q_courte, :q_longue, :importance, :nb_apparition)';
 		$req = $bdd->prepare($sql);
 		$req->bindParam(':q_courte', $q_courte);
 		$req->bindParam(':q_longue', $q_longue);
 		$req->bindParam(':importance', $importance);
-		$req->bindParam(':popularite', $popularite);
+		$req->bindParam(':nb_apparition', $nb_apparition);
 		$req->execute();
 		$id_nouveau = $bdd->lastInsertId();
 		addQuestionCategorie($id_nouveau, $categories);
@@ -42,7 +42,7 @@
 	{
 		foreach ($categoriesID as $cat) {
 			$bdd = getBdd();
-			$sql = 'INSERT INTO questions_categories VALUES(?, ?)';
+			$sql = 'INSERT INTO categorie_question VALUES(?, ?)';
 			$req = $bdd->prepare($sql);
 			$req->execute(array($questionID, $cat));
 		}
@@ -51,7 +51,7 @@
 	// Supprime une question
 	function deleteQuestion($questionID) {
 		$bdd = getBdd();
-		$sql = 'DELETE FROM questions WHERE id = '.$questionID.'';
+		$sql = 'DELETE FROM question WHERE id = '.$questionID.'';
 		$req = $bdd->prepare($sql);
 		$req->execute();
 	}
@@ -59,20 +59,20 @@
 	// Supprime une question de la table question_categorie
 	function deleteQuestionCategorie($questionID) {
 		$bdd = getBdd();
-		$sql = 'DELETE FROM questions_categories WHERE question_id = '.$questionID.'';
+		$sql = 'DELETE FROM categorie_question WHERE question_id = '.$questionID.'';
 		$req = $bdd->prepare($sql);
 		$req->execute();
 	}
 	
 	// Modifie une question
-	function changeQuestion($questionID, $q_courte, $q_longue, $importance, $popularite) {
+	function changeQuestion($questionID, $q_courte, $q_longue, $importance, $nb_apparition) {
 		$bdd = getBdd();
-		$sql = 'UPDATE questions SET question_courte=:q_courte, question_longue=:q_longue, importance=:importance, popularite=:popularite WHERE id = :questionID';
+		$sql = 'UPDATE question SET q_courte=:q_courte, q_longue=:q_longue, importance=:importance, nb_apparition=:nb_apparition WHERE id = :questionID';
 		$req = $bdd->prepare($sql);
 		$req->bindParam(':q_courte', $q_courte);
 		$req->bindParam(':q_longue', $q_longue);
 		$req->bindParam(':importance', $importance);
-		$req->bindParam(':popularite', $popularite);
+		$req->bindParam(':nb_apparition', $nb_apparition);
 		$req->bindParam(':questionID', $questionID);
 		$req->execute();
 		//changeQuestionCategorie($questionID, $categorieID);
@@ -82,7 +82,7 @@
 	function changeQuestionCategorie($questionID, $categoriesID) {
 		foreach ($categoriesID as $categoriesID):
 			$bdd = getBdd();
-			$sql = 'UPDATE questions_categories SET categorie_id=:categorieID WHERE question_id=:questionID';
+			$sql = 'UPDATE categorie_question SET categorie_id=:categorieID WHERE question_id=:questionID';
 			$req = $bdd->prepare($sql);
 			$req->bindParam(':categorieID', $categoriesID);
 			$req->bindParam(':questionID', $questionID);
@@ -94,7 +94,7 @@
 	function getQuestionCategories($questionID) {
 		$i = 0;
 		$bdd = getBdd();
-		$sql = 'SELECT categorie_id AS cat_id FROM questions_categories WHERE question_id =?';
+		$sql = 'SELECT categorie_id AS cat_id FROM categorie_question WHERE question_id =?';
 		$req = $bdd->prepare($sql);
 		if($req->execute(array($questionID))) {
 			while ($cat_id = $req->fetch()) {
@@ -110,7 +110,7 @@
 	// Renvoie le nombre d'occurences d'une question dans la table question_categorie
 	function getQuestionOccurences($questionID) {
 		$bdd = getBdd();
-		$sql = 'SELECT question_id AS id, COUNT( question_id ) AS nb_cat FROM questions_categories WHERE question_id =?';
+		$sql = 'SELECT question_id AS id, COUNT( question_id ) AS nb_cat FROM categorie_question WHERE question_id =?';
 		$req = $bdd->prepare($sql);
 		$req->execute(array($questionID));
 		if ($req->rowCount() >= 1)
@@ -129,7 +129,7 @@
 	// Renvoie toutes les catégories
 	function getCategories() {
 		$bdd = getBdd();
-		$sql = 'SELECT id AS id, nom AS nom FROM categories';
+		$sql = 'SELECT id AS id, nom AS nom FROM categorie';
 		$categories = $bdd->query($sql);
 		return $categories;
 	}
@@ -137,7 +137,7 @@
 	// Renvoie la catégorie demandée
 	function getCategorie($categorieID) {
 		$bdd = getBdd();
-		$sql = 'SELECT id AS id, nom AS nom FROM categories WHERE id =?';
+		$sql = 'SELECT id AS id, nom AS nom FROM categorie WHERE id =?';
 		$categorie = $bdd->prepare($sql);
 		$categorie->execute(array($categorieID));
 		if ($categorie->rowCount() == 1)
@@ -149,7 +149,7 @@
 	// Rajoute une catégorie
 	function addCategorie($nom) {
 		$bdd = getBdd();
-		$sql = 'INSERT INTO categories(nom) VALUES(:nom)';
+		$sql = 'INSERT INTO categorie(nom) VALUES(:nom)';
 		$req = $bdd->prepare($sql);
 		$req->bindParam(':nom', $nom);
 		$req->execute();
@@ -158,7 +158,7 @@
 	// Supprime une catégorie
 	function deleteCategorie($categorieID) {
 		$bdd = getBdd();
-		$sql = 'DELETE FROM categories WHERE id = '.$categorieID.'';
+		$sql = 'DELETE FROM categorie WHERE id = '.$categorieID.'';
 		$req = $bdd->prepare($sql);
 		$req->execute();
 	}
@@ -166,7 +166,7 @@
 	// Modifie une catégorie
 	function changeCategorie($categorieID, $nom) {
 		$bdd = getBdd();
-		$sql = 'UPDATE categories SET nom=:nom WHERE id = :categorieID';
+		$sql = 'UPDATE categorie SET nom=:nom WHERE id = :categorieID';
 		$req = $bdd->prepare($sql);
 		$req->bindParam(':nom', $nom);
 		$req->bindParam(':categorieID', $categorieID);
@@ -176,7 +176,7 @@
 	// Renvoie le nombre d'occurences d'une catégorie dans la table question_categorie
 	function getCategorieOccurences($categorieID) {
 		$bdd = getBdd();
-		$sql = 'SELECT categorie_id AS id, COUNT( categorie_id ) AS nb_cat FROM questions_categories WHERE categorie_id =?';
+		$sql = 'SELECT categorie_id AS id, COUNT( categorie_id ) AS nb_cat FROM categorie_question WHERE categorie_id =?';
 		$req = $bdd->prepare($sql);
 		$req->execute(array($categorieID));
 		if ($req->rowCount() >= 1)
@@ -187,13 +187,4 @@
 		else
 			throw new Exception("Aucune question ne correspond à l'identifiant '$categorieID'");
 	}
-	function getQuestionsby($categorie,$orderby,$sens,$limit){
-        $selector= 'SELECT ID,question_courte,question_longue,importance,nb_apparition
-        FROM question, categorie_question 
-        WHERE ID=question_id AND categorie_id=? ORDER BY ? ? LIMIT ?';
-        $reponsebdd = $bddpdo->prepare($selector);
-        $reponsebdd->execute(array($categorie,$orderby,$sens,$limit));
-        return $reponsebdd;
-
-    }
 ?>
