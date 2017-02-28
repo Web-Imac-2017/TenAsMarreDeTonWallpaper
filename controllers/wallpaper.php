6ec3ddeee9e20fb540674dc87cd3bb927b2a2dbd
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-02-28 12:17:13 +0100

    Controller wallpaper

--- controllers/wallpaper.php
+++ controllers/wallpaper.php
@@ -1,42 +1,52 @@
 <?php
-	require('db_connect.php');
-	
-	// Renvoie les informations de tous les wallpapers
-	function getWallpapers() {
-		$bdd = getBdd();
-		$sql = 'SELECT * FROM wallpaper';
-		$wallpapers = $bdd->prepare($sql);
-		$wallpapers->execute();
 
-		return $wallpapers;
+require_once "../kernel/controller.php";
+require_once "../kernel/database.php";
+
+class Welcome extends Controller {
+
+	public function __construct(){
+		parent::__construct();
 	}
-	
+
+    // Renvoie les informations de tous les wallpapers
+	public function getAll() {
+        $bdd = Database::get(); // A vérifier si cela fonctionne
+		$sql = 'SELECT * FROM wallpaper';
+		$data['content'] = $bdd->prepare($sql);
+		$data['content']->execute();
+        
+        return json_encode($data);
+	}
+    
+    
 	// Renvoie les informations d'un seul wallpaper
-	function getWallpaper($wallpaperID) {
-		$bdd = getBdd();
+	public function get($id) {
+		$bdd = Database::get();
 		$sql = 'SELECT * FROM wallpaper WHERE id=?';
-		$wallpaper = $bdd->prepare($sql);
-		$wallpaper->execute(array($wallpaperID));
-		if ($wallpaper->rowCount() == 1)
-			return $wallpaper->fetch();  // Accès à la première ligne de résultat
+		$data['content'] = $bdd->prepare($sql);
+		$data['content']->execute(array($id));
+		if ($data['content']->rowCount() == 1)
+			$data['content']->fetch();  // Accès à la première ligne de résultat
 		else
 			throw new Exception("Aucune wallpaper ne correspond à l'identifiant '$wallpaperID'");
+        return json_encode($data);
 	}
 	
 	// Ajoute un nouveau wallpaper
-	function addWallpaper($url, $estapparent, $categories) {
-		$bdd = getBdd();
+	public function add() {
+		$bdd = Database::get();
 		$sql = 'INSERT INTO wallpaper VALUES(NULL, ?, ?)';
 		$req = $bdd->prepare($sql);
-		$req->execute(array($url, $estapparent));
+		$req->execute(array($_POST['url'], 0));
 
 		$id = getIdLastWallpaper(); // on récupère l'id du nouvel wallpaper
 		setWallpaperCategories($id, $categories); // on associe le wallpaper aux différentes catégories
 	}
 
 	// Renvoie l'id du dernier wallpaper inséré
-	function getIdLastWallpaper() {
-		$bdd = getBdd();
+	public function getIdLastWallpaper() {
+		$bdd = Database::get();
 		$sql = 'SELECT id FROM wallpaper ORDER BY id DESC LIMIT 1';
 		$req = $bdd->prepare($sql);
 		$req->execute();
@@ -46,17 +56,17 @@
 	}
 	
 	// Supprime un wallpaper
-	function deleteWallpaper($wallpaperID) {
-		$bdd = getBdd();
+	public function deleteWallpaper($wallpaperID) {
+		$bdd = Database::get();
 		$sql = 'DELETE FROM wallpaper WHERE id=?';
 		$req = $bdd->prepare($sql);
 		$req->execute(array($wallpaperID));
 	}
 
 	// Associe des catégories à un wallpaper
-	function setWallpaperCategories($wallpaperID, $categories) {
+	public function setWallpaperCategories($wallpaperID, $categories) {
 		foreach ($categories as $cat) {
-			$bdd = getBdd();
+			$bdd = Database::get();
 			$sql = 'INSERT INTO WallpaperCategories VALUES(?,?)';
 			$req = $bdd->prepare($sql);
 			$req->execute(array($wallpaperID, $cat));
@@ -64,8 +74,8 @@
 	}
 	
 	// Modifie un wallpaper
-	function changewallpaper($wallpaperID, $url, $estapparent, $categories) {
-		$bdd = getBdd();
+	public function changewallpaper($wallpaperID, $url, $estapparent, $categories) {
+		$bdd = Database::get();
 		$sql = 'UPDATE Wallpaper SET url=?, estapparent=? WHERE id=?';
 		$req = $bdd->prepare($sql);
 		$req->execute(array($url, $estapparent, $wallpaperID));
@@ -75,33 +85,27 @@
 	}
 	
 	// Supprimer toutes les catégories d'un wallpaper
-	function deleteWallpaperCategories($wallpaperID) {
-		$bdd = getBdd();
+	public function deleteWallpaperCategories($wallpaperID) {
+		$bdd = Database::get();
 		$sql = 'DELETE FROM WallpaperCategories WHERE wallpaper_id=?';
 		$req = $bdd->prepare($sql);
 		$req->execute(array($wallpaperID));
 	}	
 
-	// Renvoie toutes les catégories
-	function getCategories() {
-		$bdd = getBdd();
-		$sql = 'SELECT * FROM Categorie';
-		$categories = $bdd->prepare($sql);
-		$categories->execute();
-
-		return $categories;
-	}
-
 	// Renvoie les catégories d'un wallpaper
-	function getWallpaperCategories($wallpaperID) {
-		$bdd = getBdd();
+	public function getWallpaperCategories($id) {
+		$bdd = Database::get();
 		$sql = 'SELECT * FROM WallpaperCategories INNER JOIN categorie ON categorie_id = Categorie.id WHERE wallpaper_id=?';
-		$categories = $bdd->prepare($sql);
-		$categories->execute(array($wallpaperID));
+		$data['categories'] = $bdd->prepare($sql);
+		$data['categories']->execute(array($id));
 
-		return $categories;
+		return json_encode($data);
 	}
 
+    
+}
+	
+	
 
 
 
