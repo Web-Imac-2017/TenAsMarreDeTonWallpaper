bb4e7511497fe19828e2304994be3da9b23580fb
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-02-28 12:17:37 +0100

    Controller welcome

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -10,6 +10,13 @@ class Welcome extends Controller {
 
 	public function index() {
         $data['title'] = 'Accueil';
+        
+        // Récupère les 3 wallpapers les plus populaires
+		$bdd = Database::get();
+		$sql = 'SELECT * FROM wallpaper ORDER BY compteur DESC LIMIT 3';
+		$data['3_populaires'] = $bdd->prepare($sql);
+		$data['3_populaires']->execute(array($id));
+        
         return json_encode($data);
 	}
 }
