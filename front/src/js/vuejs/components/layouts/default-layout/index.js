68d553dbdc1f0c60464ab23c326ab5d164b4b989
Thaledric <thaledric@gmail.com>
2017-03-01 20:55:02 +0100

    Mise en place de la marge du header

--- front/src/js/vuejs/components/layouts/default-layout/index.js
+++ front/src/js/vuejs/components/layouts/default-layout/index.js
@@ -5,20 +5,13 @@ import Vue from 'vue/dist/vue';
 let template = require('./template.html');
 template     = eval(`\`${template}\``);
 
+import HeaderMain from '../../sections/header-main/index.js';
+
 const DefaultLayout = Vue.extend({
   template,
 
-  props: {
-  },
-
-  data() {
-    return {
-
-    };
-  },
-
-  created() {
-      
+  components: {
+    'header-main': HeaderMain,
   }
 });
 
