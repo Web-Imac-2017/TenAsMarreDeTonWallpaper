f13e586728da0d8706f9529a8b1aa71a83614491
Thaledric <thaledric@gmail.com>
2017-03-01 21:48:27 +0100

    Header ok

--- front/src/js/vuejs/components/sections/header-main/index.js
+++ front/src/js/vuejs/components/sections/header-main/index.js
@@ -5,8 +5,14 @@ import Vue from 'vue/dist/vue';
 let template = require('./template.html');
 template     = eval(`\`${template}\``);
 
+import RainbowBar from '../../widgets/rainbow-bar/index.js';
+
 const HeaderMain = Vue.extend({
   template,
+
+  components: {
+    'rainbow-bar': RainbowBar
+  },
 });
 
 export default HeaderMain;
---
+++ front/src/js/vuejs/components/widgets/rainbow-bar/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import Vue from 'vue/dist/vue';
+
+const RainbowBar = Vue.extend({
+  template: '<div class="rainbow-bar"></div>'
+});
+
+export default RainbowBar;
