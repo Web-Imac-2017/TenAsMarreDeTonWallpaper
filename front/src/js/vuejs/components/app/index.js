47c86b90a09bfa27a361f45bf90a5f815c97281b
Thaledric <thaledric@gmail.com>
2017-03-02 13:23:58 +0100

    Sidebar class & animations

--- front/src/js/vuejs/components/app/index.js
+++ front/src/js/vuejs/components/app/index.js
@@ -6,7 +6,20 @@ let template = require('./template.html');
 template     = eval(`\`${template}\``);
 
 const App = Vue.extend({
-  template
+  template,
+
+  data(){ return{
+    preload: true
+  };},
+
+  mounted: function(){
+    // preload empêche les animations CSS de s'effectuer au chargement de la page.
+    // Une fois le component monté, on retire preload pour rendre aux éléments leurs animations.
+    let varthis = this;
+    setTimeout(function(){
+        varthis.preload = false;
+    },400);
+  }
 });
 
 export default App;
--- front/src/js/vuejs/components/layouts/default-layout/index.js
+++ front/src/js/vuejs/components/layouts/default-layout/index.js
@@ -10,6 +10,10 @@ import HeaderMain from '../../sections/header-main/index.js';
 const DefaultLayout = Vue.extend({
   template,
 
+  data(){ return{
+    sidebarOpen: false
+  };},
+
   components: {
     'header-main': HeaderMain,
   }
