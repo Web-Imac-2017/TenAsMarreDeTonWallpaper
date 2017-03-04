c6f6368596b039f70443a8c3ca04b974a829f0f2
Davdouv <d4vid.n@hotmail.fr>
2017-03-04 22:43:08 +0100

    functions nextQuestion et answerQuestion

--- models/scenario_functions.php
+++ models/scenario_functions.php
@@ -38,66 +38,100 @@
 			$other,
 			$categories_id
 		);
-		$firstQuestion = array($question, $reponses, $values);
+		$firstQuestion = array("question"=>$question, "reponses"=>$reponses, "values"=>$values);
 		return $firstQuestion;
 	}
-
-	function answerFirstQuestion($value) {
-		return $value;
-	}
 	
-	function updateImportance($importance)
-	{
-	}
-	
-	/* GESTION DES QUESTIONS SUIVANTES QUESTION */
+	/* GESTION DES QUESTIONS SUIVANTES */
 	
 	// Renvoie 2 questions en fonction de la categorie et de l'importance
 	function nextQuestion($categories, $importance)
 	{
+		$importanceUp = $importance+5;
 		$bdd = getBdd();
 		// On select les questions qui ont les categories choisies et l'importance demandée
 		$sql = "SELECT DISTINCT id, nb_apparition AS nb_a, q_longue FROM question AS q 
 				INNER JOIN categorie_question AS c_q 
 				ON q.id = c_q.question_id
-				WHERE q.importance >= :importance AND ";
-		// S'l y a plusieurs catégories
+				WHERE (q.importance >= :importance AND q.importance <= ".$importanceUp.") AND (";
+		// S'il y a plusieurs catégories, on concaténe avec des OR
 		if (gettype($categories) == "array")
 		{
 			foreach($categories as $categories) {
 				$sql .= "c_q.categorie_id = ".$categories." OR ";
 			}
 			$sql = substr($sql, 0, -4);	// Pour enlever le dernier " OR "
+			$sql .= ")";
 		}
 		else
 		{
-			$sql .= "c_q.categorie_id = ".$categories."";
+			$sql .= "c_q.categorie_id = ".$categories.")";
 		}
-		$question = $bdd->prepare($sql);
-		$question->bindParam(':importance', $importance);
-		$question->execute();
-		$selection = $question->fetchAll();
+		$req = $bdd->prepare($sql);
+		$req->bindParam(':importance', $importance);
+		$req->execute();
+		$selection = $req->fetchAll();
 		$nb_q = count($selection);
-		// S'il y a plus de 3 questions selectionnées, on en tire 3 au hasard
-		if ($nb_q > 3)
+		if ($nb_q > 0)
 		{
-			$random_question=array_rand($selection,3);
+			$selected = array();
+			$questions = array();
+			$id = array();
+			// S'il y a plus de 3 questions selectionnées, on en tire 3 au hasard
+			if ($nb_q > 3)
+			{
+				$random_question=array_rand($selection,3);
+				$nb = 3;
+			}
+			else {
+				$random_question=array(0,1,2);
+				$nb = $nb_q;
+			}
+			for ($i = 0; $i < $nb; $i++)
+			{
+				array_push($selected, $selection[$random_question[$i]]);
+				array_push($questions, $selection[$i]['q_longue']);
+				array_push($id, $selection[$i]['id']);
+			}
+			$nextQuestion = array('nb_q'=>$nb_q, 'questions'=>$questions, 'id'=>$id);
 		}
-		else {
-			$random_question=array(0,1,2);
-		}
-		$selected = array();
-		$questions = array();
-		$id = array();
-		for ($i = 0; $i < $nb_q; $i++)
+		// S'il n'y a aucune question qui correspond à la requête
+		else
 		{
-			array_push($selected, $selection[$random_question[$i]]);
-			array_push($questions, $selection[$i]['q_longue']);
-			array_push($id, $selection[$i]['id']);
+			$nextQuestion = array('nb_q'=>0,'questions'=>array("Aucune question"),'id'=>array(0));
 		}
-		asort($selected);
-		print_r($selected);
-		$nextQuestion = array($questions, $id);
+
 		return $nextQuestion;
+	}
+	
+	/* GESTION DES REPONSES */
+	
+	// Renvoie le nombre de wpp correspondant à la requete actuelle
+	function answerQuestion($question_id, $reponse, $requete)
+	{
+		$wppLeft;
+		
+		$bdd = getBdd();
+		// On select les wpp qui correspondent aux reponses choisies
+		$sql = "SELECT DISTINCT wallpaper_id FROM reponse AS r 
+				WHERE question_id =".$question_id."
+				AND (val_min <=".$reponse." AND val_max>=".$reponse.")";
+		$req = $bdd->prepare($sql);
+		$req->execute();
+		$selection = $req->fetchAll(PDO::FETCH_ASSOC);
+		
+		$nb_wpp_left = count($selection);
+		$wppLeft = array("nb_wpp_left"=>$nb_wpp_left, "id"=>$selection, "requete"=>$requete);
+		
+		return $wppLeft;
+	}
+	
+	/* GESTION DES CONDITIONS D'ARRET */
+	
+	// Renvoie le nombre de wpp correspondant à la requete actuelle
+	function stopGame($wpp_id)
+	{		
+		
+		return $wpp_id;
 	}
 ?>
