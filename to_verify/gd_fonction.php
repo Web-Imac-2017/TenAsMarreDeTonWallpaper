3957b56c28703f25804f4c8a66948d54257ce260
Kenthyvong Tan <kenthyvong.tan@gmail.com>
2017-03-06 18:45:50 +0100

    GD fonction

--- to_verify/gd_fonction.php
+++ to_verify/gd_fonction.php
@@ -1,10 +1,75 @@
 <?php
+//OBJECTIF
+//je télécharge le wallpaper depuis une url internet et crée sa miniature
+//j'ouvre un wallpaper existant depuis un fichier
+//je récupère ses info
+//je le traite
 
+//nb : lorsque imagejpeg ou imagepng possède un second argument, il n'affiche rien
+
+//les trois fonctions marchent avec path et url !!! yeaaaah
 function gdgetpic($url){
     return ImageCreateFromString(file_get_contents($url));
 }
-function gdresize($url,$ratio){
+function gdcollect($url){
     $image=gdgetpic($url);
+    $info=getimagesize($url);
+    $format=image_type_to_extension($info[2]);
+
+    return array("image"=>$image, "height"=>$info[1],"width"=>$info[0],"extension"=>$format);
+}
+
+//n'affiche rien
+function gdsave($image,$filename,$dotextension){
+    switch($dotextension){
+        case ".jpeg":
+        case ".jpg":{
+            //echo "c'est un jpeg";
+            ImageJPEG($image,$filename.$dotextension);
+        }break;
+        case ".png":{
+            //echo "cest un png";
+            Imagepng($image,$filename.$dotextension);
+        }break;
+        default:
+            break;
+    }
+
+}
+
+function gdcrop($image,$startx,$starty,$cropwidth,$cropheight){
+    
+    $output=ImageCreateTrueColor($cropwidth, $cropheight);
+    ImageCopyResampled($output, $image, 0, 0, $startx, $starty, $cropwidth, $cropheight, $cropwidth, $cropheight);
+
+    return $output;
+}
+//argument supplémentaire après image:
+//seulement image -> thumbnail
+//width -> resize avec la width demandé proportionellement
+//widht + height -> simple resize
+function gdresize($image, $width = 150, $height = true) {
+
+    $height=($height === true)? (ImageSY($image)*$width/ImageSX($image)):$height;
+
+    $output=ImageCreateTrueColor($width, $height);
+    ImageCopyResampled($output, $image, 0, 0, 0, 0, $width, $height, ImageSX($image), ImageSY($image));
+
+    return $output;
+}
+
+//Decommenter les 2 lignes suivantes et commenter celle de print_r pour visualiser l'image
+header ("Content-type: image/jpg");
+$res=gdcollect("http://cdn.cutestpaw.com/wp-content/uploads/2013/01/l-Cute-Kitten.jpg");
+gdsave($res["image"],"phototest",$res["extension"]);
+
+$recup=gdcollect("phototest.jpeg");
+imagejpeg($recup["image"]);
+
+//print_r(gdcollect("http://cdn.cutestpaw.com/wp-content/uploads/2013/01/l-Cute-Kitten.jpg"));
+//Array ( [image] => Resource id #4 [height] => 435 [width] => 580 [extension] => .jpeg ) 
+
+/*function gdresize($image,$ratio){
     
     $height=ImageSY($image)*$ratio;
     $width=ImageSX($image)*$ratio;
@@ -12,44 +77,7 @@ function gdresize($url,$ratio){
     $output=ImageCreateTrueColor($width, $height);
     ImageCopyResampled($output, $image, 0, 0, 0, 0, $width, $height, ImageSX($image), ImageSY($image));
 
-    ImageJPEG($output); 
-
     return $output;
-}
-function gdcrop($url,$startx,$starty,$cropwidth,$cropheight){
-    $image=gdgetpic($url);
-    
-    $output=ImageCreateTrueColor($cropwidth, $cropheight);
-    ImageCopyResampled($output, $image, 0, 0, $startx, $starty, $cropwidth, $cropheight, $cropwidth, $cropheight);
+}*/
 
-    ImageJPEG($output); 
-
-    return $output;
-}
-function gdthumbnail($url, $filename, $width = 150, $height = true) {
-    $image=gdgetpic($url);
-    
-    $height=($height == true)? (ImageSY($image)*$width/ImageSX($image)):$height;
-
-    $output=ImageCreateTrueColor($width, $height);
-    ImageCopyResampled($output, $image, 0, 0, 0, 0, $width, $height, ImageSX($image), ImageSY($image));
-
-    ImageJPEG($output, $filename, 95); 
-
-    return $output;
-}
-
-/*ImageJPEG(gdcrop("http://cdn.cutestpaw.com/wp-content/uploads/2013/01/l-Cute-Kitten.jpg",200,200,100,100));
-header ("Content-type: image/jpeg"); 
-*/
-header ("Content-type: image/png");
-$image = imagecreate(200,50);
-
-$orange = imagecolorallocate($image, 255, 128, 0);
-$bleu = imagecolorallocate($image, 0, 0, 255);
-$bleuclair = imagecolorallocate($image, 156, 227, 254);
-$noir = imagecolorallocate($image, 0, 0, 0);
-$blanc = imagecolorallocate($image, 255, 255, 255);
-
-imagepng($image);
 ?>
