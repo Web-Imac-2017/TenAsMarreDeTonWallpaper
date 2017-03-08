4f97bb1afe66919f7567a4ee35fee250cbc792e1
Davdouv <d4vid.n@hotmail.fr>
2017-03-08 20:51:01 +0100

    OK JE PUSH L'ALGO o/

--- models/scenario_functions.php
+++ models/scenario_functions.php
@@ -16,7 +16,7 @@
 			$categories[$random_categorie[1]]['nom'],
 			$categories[$random_categorie[2]]['nom'],
 			"Rien de tout ça",
-			"Surprend moi !"
+			"Surprenez-moi !"
 		);
 		// On conserve uniquement les id des catégories dans un array
 		$categories_id = array();
@@ -24,7 +24,7 @@
 		{
 			array_push($categories_id, $categories['id']);
 		}
-		// On stocke toutes les catégories sauf les 3 tirées au hasard
+		// On stocke toutes les catégories sauf les 2 tirées au hasard
 		$other = $categories_id;
 		for ($i = 2; $i >= 0; $i--)
 		{
@@ -38,6 +38,7 @@
 			$other,
 			$categories_id
 		);
+		shuffle($other);
 		$firstQuestion = array("question"=>$question, "reponses"=>$reponses, "values"=>$values);
 		return $firstQuestion;
 	}
@@ -45,18 +46,21 @@
 	/* GESTION DES QUESTIONS SUIVANTES */
 	
 	// Renvoie 2 questions en fonction de la categorie et de l'importance
-	function nextQuestion($categories, $importance)
-	{
+	function nextQuestion()
+	{		
 		// S'il y a plusieurs catégories, on en sélectionne 1 au hasard
-		if (gettype($categories) == "array")
+		if (gettype($_SESSION['categories']) == "array")
 		{
-			$categorie = $categories[array_rand($categories,1)];
+			$categorie = $_SESSION['categories'][array_rand($_SESSION['categories'],1)];
 		}
 		else
 		{
-			$categorie = $categories;
+			$categorie = $_SESSION['categories'];
 		}
-		$importanceUp = $importance+5;
+		// On fixe une marge pour l'importance
+		$importanceUp = $_SESSION['importance']+5;
+		
+		// On effectue la requete SQL qui récupére la question
 		$bdd = getBdd();
 		// On select les questions qui ont les categories choisies et l'importance demandée
 		$sql = "SELECT DISTINCT id, nb_apparition AS nb_a, q_longue FROM question AS q 
@@ -65,41 +69,50 @@
 				WHERE (q.importance >= :importance AND q.importance < ".$importanceUp.")
 				AND (c_q.categorie_id = :categorie)";
 		$req = $bdd->prepare($sql);
-		$req->bindParam(':importance', $importance);
+		$req->bindParam(':importance', $_SESSION['importance']);
 		$req->bindParam(':categorie', $categorie);
 		$req->execute();
-		$selection = $req->fetchAll();
+		// On récupére le résultat dans un tableau $selection
+		$selection = $req->fetchAll(PDO::FETCH_ASSOC);
+		// On compte combien de question on a trouvé
 		$nb_q = count($selection);
-		// Si on trouve au moins une question on stock les infos dans des tableaux
+		
+		// Si on trouve au moins une question on en tire une au hasard et on la renvoie
 		if ($nb_q > 0)
 		{
-			$selected = array();
-			$questions = array();
-			$id = array();
-			$nb_a = array();
-			// S'il y a plus de 3 questions selectionnées, on en tire 3 au hasard
-			if ($nb_q > 3)
+			// Si on en trouve seulement une
+			if ($nb_q == 1)
 			{
-				$random_question=array_rand($selection,3);
-				$nb = 3;
+				$random_question=array_rand($selection,1);
+				$selected = $selection[$random_question];
 			}
-			else {
-				$random_question=array(0,1,2);
-				$nb = $nb_q;
-			}
-			for ($i = 0; $i < $nb; $i++)
+			// Si on en trouve plus d'une
+			else
 			{
-				array_push($selected, $selection[$random_question[$i]]);
-				array_push($questions, $selection[$i]['q_longue']);
-				array_push($id, $selection[$i]['id']);
-				array_push($nb_a, $selection[$i]['nb_a']);
+				// On en tire 2 au hasard
+				$random_question=array_rand($selection,2);
+				// On selectionne la question qui est apparue le moins de fois
+				if ($selection[$random_question[0]]['nb_a'] <= $selection[$random_question[1]]['nb_a'])
+				{
+					$selected = $selection[$random_question[0]];
+				}
+				else
+				{
+					$selected = $selection[$random_question[1]];	
+				}
 			}
-			$nextQuestion = array('nb_q'=>$nb_q, 'questions'=>$questions, 'id'=>$id, 'nb_a'=>$nb_a);
+			$nextQuestion = array('nb_q'=>$nb_q, 'question'=>$selected);
+			// Si c'est la 2ème question, on update le nombre d'apparition ici, sinon ça se fera dans une autre fonction
+			if ($_SESSION['num_question'] == 2)
+			{
+				updateNb_aQ($selected['id'],$selected['nb_a']);
+			}
 		}
 		// S'il n'y a aucune question qui correspond à la requête
 		else
 		{
-			$nextQuestion = array('nb_q'=>0,'questions'=>array("Aucune question"),'id'=>array(0),'nb_a'=>array(0));
+			$nextQuestion = array('nb_q'=>0,'question'=>NULL);
+			$_SESSION['continue'] = false;
 		}
 
 		return $nextQuestion;
@@ -131,7 +144,9 @@
 		if ($requete)
 		{
 			$sql .= " AND wallpaper_id IN ( ".$requete." )";
+			// Ou alors faire une série de AND ... AND ...  (plus performant)
 		}
+
 		$req = $bdd->prepare($sql);
 		$req->execute();
 		$selection = $req->fetchAll(PDO::FETCH_ASSOC);		
@@ -148,7 +163,7 @@
 	
 	/* GESTION DES CONDITIONS D'ARRET */
 	
-	// Renvoie le nombre de wpp correspondant à la requete actuelle
+	// Renvoie les informations des wpp trouvés
 	function stopGame($wpp_id)
 	{		
 		$i = 0;
@@ -156,6 +171,7 @@
 		$bdd = getBdd();
 		foreach ($wpp_id as $id)
 		{
+			// On récupére les informations
 			$sql = 'SELECT * FROM wallpaper WHERE id=:id';
 			$req = $bdd->prepare($sql);
 			$req->bindParam(':id', $id);
@@ -163,20 +179,23 @@
 			$resultat = $req->fetchAll(PDO::FETCH_ASSOC);
 			array_push($selection, $resultat[0]);
 			
+			// On met à jour le nombre d'apparition des wpp
 			$nb_apparition = $selection[$i]['nb_apparition']+1;
 			$sql2 = 'UPDATE wallpaper SET nb_apparition=:nb_apparition WHERE id=:id';
 			$req2 = $bdd->prepare($sql2);
 			$req2->bindParam(':nb_apparition', $nb_apparition);
-			$req2->bindParam(':id', $id);
+			$req2->bindParam(':id', $i);
 			$req2->execute();
 			
 			$i++;
 		}
-		
+		// On rend la selection aléatoire
+		shuffle($selection);
+		// On renvoie les wpp sélectionnés
 		return $selection;
 	}
 	
-	// Check si la question peut fournir des wpp
+	// Check si la question peut fournir des wpp (appelle la fonction nextQuestion et les fonctions de test)
 	function checkQuestion($question_id, $requete)
 	{		
 		$wpp_left = array();
@@ -188,7 +207,7 @@
 			$wpp = answerQuestion($question_id, $reponse, $requete);
 			array_push($wpp_left, $wpp['nb_wpp_left']);
 		}
-		print_r($wpp_left);
+		
 		// Si il y a suffisamment de wpp pour chaque reponse possible, on continue
 		if ($wpp_left[0] >= $nb_min && $wpp_left[1] >= $nb_min && $wpp_left[2] >= $nb_min && $wpp_left[3] >= $nb_min && $wpp_left[4] >= $nb_min)
 		{
@@ -203,9 +222,118 @@
 		return $checkQuestion;
 	}
 	
-	// Incremente l'importance de -5
-	function updateImportance($importance)
+	// Incremente l'importance (mettre -1 pour -5 | 1 pour 5)
+	function updateImportance($sens)
 	{
-		return $importance-5;
+		$_SESSION['importance']=$_SESSION['importance']+(5*$sens);
+	}
+	
+	// Retourne la question actuelle
+	function getCurrentQuestion()
+	{
+		return $_SESSION['question'][$_SESSION['num_question']];
+	}
+	
+	// Check les conditions d'arrêts, dis si on peut continuer ou pas
+	// Retourne la nouvelle question et met à jour les variables (num_question, importance, requete)
+	function checkContinue($reponse)
+	{
+		// Variables retournées avec initialisation si jamais elles ne sont pas modifiées
+		$nb_wpp_left = 0;
+		$wallpapers = array();
+		
+		// On récupére la valeur de la réponse
+		$_SESSION['reponse'] = $reponse;
+
+		// On compte combien de wpp on trouve et on met à jour la requete
+		$wppLeft = answerQuestion($_SESSION['question'][$_SESSION['num_question']-1]['question']['id'],$_SESSION['reponse'],$_SESSION['requete'][$_SESSION['num_question']-2]);
+		
+		// Si on trouve moins de $minWPP wpp, on arrête
+		if($wppLeft['nb_wpp_left']<=$_SESSION['minWPP'] || $_SESSION['num_question'] == $_SESSION['maxQuestion'])
+		{
+			$_SESSION['continue'] = false;
+			//echo "Il n'y a plus que ".$wppLeft['nb_wpp_left']." wallpapers qui correspondent à vos critères";
+			// On récupére les infos de tous les wpp qu'il reste
+			$wallpapers = stopGame($wppLeft['id']);
+			$nb_wpp_left = $wppLeft['nb_wpp_left'];
+		}
+		
+		// Sinon on passe à la question suivante
+		else
+		{
+			// On met à jour l'importance
+			updateImportance(-1);
+						
+			// Si on n'est pas dans le cas "undo", on cherche une nouvelle question (sinon elle a déjà été trouvée)
+			if ($_SESSION['lock'][$_SESSION['num_question']]==false)
+			{	
+				$_SESSION['lock'][$_SESSION['num_question']] = true;
+				// On prépare la prochaine question
+				$_SESSION['question'][$_SESSION['num_question']] = nextQuestion();
+			}
+				
+			// On met à jour la requete
+			$_SESSION['requete'][$_SESSION['num_question']-1] = $wppLeft['requete'];
+				
+			// On check si on peut trouver des wpp avec la prochaine question
+			$checkQuestion = checkQuestion($_SESSION['question'][$_SESSION['num_question']]['question']['id'], $_SESSION['requete'][$_SESSION['num_question']-1]);
+
+			// Si on ne peut pas, on arrête
+			if($checkQuestion['continue']==false)
+			{
+				$_SESSION['continue'] = false;
+				//echo "La question suivante ne permet pas de trouver assez de wallpapers";
+				$wallpapers = stopGame($wppLeft['id']);
+				// S'il y en a moins que le minimum, la limite = le nombre de wpp qu'il reste
+				if($wppLeft['nb_wpp_left']<$_SESSION['minWPP'])
+				{
+					$nb_wpp_left = $wppLeft['nb_wpp_left'];
+				}
+				// S'il y en a plus que le minimum, on affiche uniquement le minimum de wpp
+				else
+				{
+					$nb_wpp_left = $_SESSION['minWPP'];
+				}
+			}
+			// Sinon on peut continuer, on incremente le nb_apparition de la question
+			else 
+			{
+				// On incrémente le numero de la question
+				$_SESSION['num_question']++;
+				$_SESSION['continue'] = true;
+				updateNb_aQ($_SESSION['question'][$_SESSION['num_question']-1]['question']['id'],$_SESSION['question'][$_SESSION['num_question']-1]['question']['nb_a']);
+			}
+		}
+		$resultat = array('nb_wpp_left' => $nb_wpp_left, 'wallpapers' => $wallpapers);
+		return $resultat;
+	}
+	
+	// Corrige la question précédente
+	function undoQuestion()
+	{
+		if ($_SESSION['continue'] == true)
+		{
+			$_SESSION['num_question']--;
+		}
+		$_SESSION['continue'] = true;
+		if ($_SESSION['num_question'] > 1)
+		{
+			updateImportance(1);
+		}
+		return true;
+	}
+	
+	// RENVOYER UN RETURN CODE
+	
+	// Met à jour le nombre de téléchargements du wallpaper
+	function updateDL($wallpaper_id, $nb_telechargement)
+	{
+		$nb_telechargement++;
+		$bdd = getBdd();
+		$sql = 'UPDATE wallpaper SET nb_telechargement=:nb_telechargement WHERE id=:wallpaper_id';
+		$req = $bdd->prepare($sql);
+		$req->bindParam(':nb_telechargement', $nb_telechargement);
+		$req->bindParam(':wallpaper_id', $wallpaper_id);
+		$req->execute();
 	}
 ?>
