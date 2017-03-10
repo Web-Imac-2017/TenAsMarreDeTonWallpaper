e76b15c2675970b08f4df0ffe6615b29192b0e7a
Yoan Lecoq <yoanlecoq.io@gmail.com>
2017-03-10 17:40:44 +0100

    Ajouté isInt(). Vérif' des entrées.

--- front/src/js/vuejs/components/widgets/dlwpp/index.js
+++ front/src/js/vuejs/components/widgets/dlwpp/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Vue from 'vue/dist/vue';
+import {isInt} from '../../../utils/isInt';
 
 let template = require('./template.html');
 template     = eval(`\`${template}\``);
@@ -18,9 +19,13 @@ const DlWpp = Vue.extend({
       prev: function() { alert("TODO Wallpaper précédent !"); },
       next: function() { alert("TODO Wallpaper suivant !"); },
       telecharger: function() {
-        alert("TODO Télécharger wallpaper (" +
-            this.$data.largeur + " x " + this.$data.hauteur + ")"
-        );
+          let l = this.$data.largeur;
+          let h = this.$data.hauteur;
+          if(!(isInt(l) || isInt(h)) || l==0 || h==0) {
+              alert("XXX Entrées invalides !");
+              return false;
+          }
+          alert("TODO Télécharger wallpaper (" + l + " x " + h + ")");
       }
     }
 });
