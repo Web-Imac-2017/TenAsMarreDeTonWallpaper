147c93b18ae0f4a796e0ce294813b2160be664f2
Yoan Lecoq <yoanlecoq.io@gmail.com>
2017-03-10 17:27:37 +0100

    Two-way binding avec v-model.

--- front/src/js/vuejs/components/widgets/dlwpp/index.js
+++ front/src/js/vuejs/components/widgets/dlwpp/index.js
@@ -6,18 +6,23 @@ let template = require('./template.html');
 template     = eval(`\`${template}\``);
 
 const DlWpp = Vue.extend({
-  template,
+    template,
 
-methods: {
-  fermer: function() {
-    alert("TODO fermer ce widget !");
-  },
-  prev: function() {
-    alert("TODO Wallpaper précédent !");
-  },
-  next: function() {
-    alert("TODO Wallpaper suivant !");
-  }}
+    data: () => ({
+        // Pas 0, sinon le placeholder ne s'affiche pas.
+        largeur:"", hauteur:""
+    }),
+
+    methods: {
+      fermer: function() { alert("TODO fermer ce widget !"); },
+      prev: function() { alert("TODO Wallpaper précédent !"); },
+      next: function() { alert("TODO Wallpaper suivant !"); },
+      telecharger: function() {
+        alert("TODO Télécharger wallpaper (" +
+            this.$data.largeur + " x " + this.$data.hauteur + ")"
+        );
+      }
+    }
 });
 
 export default DlWpp;
