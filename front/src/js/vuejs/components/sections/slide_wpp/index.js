a0ebf8369c562513664841f366044fd72286f898
MickaelLHAridon <mickael.lharidon@gmail.com>
2017-03-11 20:29:53 +0100

    bande slider home

---
+++ front/src/js/vuejs/components/sections/slide_wpp/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import Vue from 'vue/dist/vue';
+
+let template = require('./template.html');
+template     = eval(`\`${template}\``);
+
+const Slide_wpp = Vue.extend({
+  template,
+
+  props: {
+    'wallpapers-height-rem': { type: Number, default: function(){ return 6; }}
+  },
+
+  data() {
+    return {
+    	wallpapers:[
+			{ thumbnail: '/TenAsMarreDeTonWallpaper/media/wallpapers/sunset.jpg', width: 1920, height: 1080 },
+            { thumbnail: '/TenAsMarreDeTonWallpaper/media/wallpapers/nyan-cat.jpg', width: 1920, height: 1080 },
+            { thumbnail: '/TenAsMarreDeTonWallpaper/media/wallpapers/rain.png', width: 2000, height: 1500 }     
+        ]
+        index: 0
+    };
+  },
+
+  computed: {
+    visible() {
+        return this.index === this.parent.index
+    }
+  }
+
+  methods: {
+    getThumbnailCss(wallpaper){
+        let ret = {
+            'background-image': 'url('+wallpaper.thumbnail+')',
+            'width': (wallpaper.width / wallpaper.height * this.wallpapersHeightRem)+'rem',
+            'height': this.wallpapersHeightRem+'rem'
+        };
+        return ret;
+    }
+  },
+
+
+});
+
+export default Slide_wpp;
+
--- front/src/js/vuejs/components/sections/slider-wpp-home/index.js
+++ front/src/js/vuejs/components/sections/slider-wpp-home/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Vue from 'vue/dist/vue';
+import Slide from '../slide_wpp/index.js';
 
 let template = require('./template.html');
 template     = eval(`\`${template}\``);
@@ -8,12 +9,45 @@ template     = eval(`\`${template}\``);
 const Slider_wpp_home = Vue.extend({
   template,
 
+  components: {
+        "slide": Slide
+    },
+
+  props: {
+    'wallpapers-height-rem': { type: Number, default: function(){ return 6; }}
+  },
+
   data() {
     return {
+    	index: 0
+    	slides: []
     };
   },
+
+  computed: {
+  	slidesCount() { return this.slides.length }
+  },
+
+  mounted () {
+  	this.slides = this.children
+  	this.slides.forEach((slide,i) => {
+  		slide.index = i
+  	})
+  },
+
+  methods: {
+    next() {
+    	this.index++
+    	if (this.index >= this.slidesCount) { this.index = 0 }
+    },
+    prev() {
+    	this.index--
+    	if (this.index < 0 ) { this.index = this.slidesCount - 1 }
+    }
+  }
 
 
 });
 
 export default Slider_wpp_home;
+
