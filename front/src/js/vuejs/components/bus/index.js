6f09149c1971d9f0bd12df6d2fa829ea435881a9
Yoan Lecoq <yoanlecoq.io@gmail.com>
2017-03-12 13:06:13 +0100

    Ajouté un truc joli à afficher en attendant d'avoir les avatars

--- front/src/js/vuejs/components/bus/index.js
+++ front/src/js/vuejs/components/bus/index.js
@@ -7,12 +7,16 @@ const Bus = Vue.extend({
   data(){ return{
     userLoggedIn: null,    // objet contenant les information de l'utilisateur connecté. null si aucun utilisateur connecté.
     //userLoggedIn: {id: 42, pseudo: 'Thaledric', admin: true, moderateur: true, mail: 'thaledric@gmail.com', avatar: '/TenAsMarreDeTonWallpaper/media/avatars/thaledric.png'},
-    headerLinks: {}
+    headerLinks: {},
+    altAvatar : "/TenAsMarreDeTonWallpaper/www/assets/icons/user-white.png"
   };},
 
   computed: {
     isUserLoggedIn: function(){
       return this.userLoggedIn != null;
+    },
+    avatar : function() {
+        return this.userLoggedIn.avatar ? this.userLoggedIn.avatar : this.altAvatar;
     }
   },
 
