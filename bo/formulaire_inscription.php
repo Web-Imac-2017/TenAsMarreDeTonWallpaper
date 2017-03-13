550bf41206a314e77557f99335024d247625eba3
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-03-13 11:25:11 +0100

    BO

--- bo/formulaire_inscription.php
+++ bo/formulaire_inscription.php
@@ -1,36 +1,32 @@
 <?php
-require('header.php');
+$page['title'] = "Inscription";
+include('header.php');
 ?>
-<body>
-    <div class="container">
 
-        <h1>S'inscrire</h1>
-        <hr>
-
-        <form action="../api/membre/add" method="post">
-            <table>
-                <tr>
-                    <td>Pseudo<span style="color:red;">*</span> :</td>
-                    <td><input type="text" name="pseudo" class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td>Mot de passe<span style="color:red;">*</span> :</td>
-                    <td><input type="password" name="password" class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td>Confirmer mot de passe<span style="color:red;">*</span> :</td>
-                    <td><input type="password" name="password2" class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td>Adresse email<span style="color:red;">*</span> :</td>
-                    <td><input type="email" name="mailAdress" class="form-control" /></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="submit" value="S'inscrire" name="submit" class="btn btn-primary"/></td>
-                </tr>
-            </table>
-        </form>
-    </div>
+    <form action="../api/membre/add" method="post">
+        <table>
+            <tr>
+                <td>Pseudo<span style="color:red;">*</span> :</td>
+                <td><input type="text" name="pseudo" class="form-control" /></td>
+            </tr>
+            <tr>
+                <td>Mot de passe<span style="color:red;">*</span> :</td>
+                <td><input type="password" name="password" class="form-control" /></td>
+            </tr>
+            <tr>
+                <td>Confirmer mot de passe<span style="color:red;">*</span> :</td>
+                <td><input type="password" name="password2" class="form-control" /></td>
+            </tr>
+            <tr>
+                <td>Adresse email<span style="color:red;">*</span> :</td>
+                <td><input type="email" name="mailAdress" class="form-control" /></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="S'inscrire" name="submit" class="btn btn-primary"/></td>
+            </tr>
+        </table>
+    </form>
+</div>
 </body>
 </html>
