66a7b2fbf8983cb4da891fca4da66cfb4e986b9a
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-03-13 11:02:24 +0100

    Améliorations BO

--- bo/formulaire_inscription.php
+++ bo/formulaire_inscription.php
@@ -1,47 +1,36 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require('header.php');
+?>
+<body>
+    <div class="container">
 
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <h1>S'inscrire</h1>
+        <hr>
 
-        <title>BO | Formulaire inscription</title>
-
-    </head>
-    <body>
-        <div class="container">
-
-            <h1>S'inscrire</h1>
-            <hr>
-
-            <form action="../api/membre/add" method="post">
-                <table>
-                    <tr>
-                        <td>Pseudo<span style="color:red;">*</span> :</td>
-                        <td><input type="text" name="pseudo" class="form-control" /></td>
-                    </tr>
-                    <tr>
-                        <td>Mot de passe<span style="color:red;">*</span> :</td>
-                        <td><input type="password" name="password" class="form-control" /></td>
-                    </tr>
-                    <tr>
-                        <td>Confirmer mot de passe<span style="color:red;">*</span> :</td>
-                        <td><input type="password" name="password2" class="form-control" /></td>
-                    </tr>
-                    <tr>
-                        <td>Adresse email<span style="color:red;">*</span> :</td>
-                        <td><input type="email" name="mailAdress" class="form-control" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type="submit" value="S'inscrire" name="submit" class="btn btn-primary"/></td>
-                    </tr>
-                </table>
-            </form>
-        </div>
-    </body>
+        <form action="../api/membre/add" method="post">
+            <table>
+                <tr>
+                    <td>Pseudo<span style="color:red;">*</span> :</td>
+                    <td><input type="text" name="pseudo" class="form-control" /></td>
+                </tr>
+                <tr>
+                    <td>Mot de passe<span style="color:red;">*</span> :</td>
+                    <td><input type="password" name="password" class="form-control" /></td>
+                </tr>
+                <tr>
+                    <td>Confirmer mot de passe<span style="color:red;">*</span> :</td>
+                    <td><input type="password" name="password2" class="form-control" /></td>
+                </tr>
+                <tr>
+                    <td>Adresse email<span style="color:red;">*</span> :</td>
+                    <td><input type="email" name="mailAdress" class="form-control" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="S'inscrire" name="submit" class="btn btn-primary"/></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
 </html>
--- bo/formulaire_wallpaper.php
+++ bo/formulaire_wallpaper.php
@@ -1,97 +1,93 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require('header.php');
+?>
+<body>
+    <div class="container">
 
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <h1>Formulaire wallpaper</h1>
+        <h4>Ajouter un nouveau wallpaper</h4>
+        <hr>
 
-        <title>BO | Formulaire wallpaper</title>
+        <form action="../api/wallpaper/add" enctype="multipart/form-data" method="post">
+            <table>
+                <tr>
+                    <td>Nom <span style="color:red;">*</span>:</td>
+                    <td><input type="text" name="nom" class="form-control" /></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Auteur :</td>
+                    <td><input type="text" name="auteur" class="form-control" /></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Upload <span style="color:red;">*</span> :</td>
+                    <td><input type="file" name="image" required /></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Catégories <span style="color:red;">*</span> :</td>
+                    <td id="cat"></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Questions :</td>
+                    <td id="rep"></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Ajouter" name="submit" class="btn btn-primary"/></td>
+                    <td></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 
-    </head>
-    <body>
-        <div class="container">
-
-            <!--<h1>Formulaire wallpaper</h1>-->
-            <h4>Ajouter un nouveau wallpaper</h4>
-            <hr>
-
-            <form action="../api/wallpaper/add" enctype="multipart/form-data" method="post">
-                <table>
-                    <tr>
-                        <td>Nom <span style="color:red;">*</span>:</td>
-                        <td><input type="text" name="nom" class="form-control" /></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Auteur :</td>
-                        <td><input type="text" name="auteur" class="form-control" /></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Upload <span style="color:red;">*</span> :</td>
-                        <td><input type="file" name="image" required /></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Catégories <span style="color:red;">*</span> :</td>
-                        <td id="cat"></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Questions :</td>
-                        <td id="rep"></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type="submit" value="Ajouter" name="submit" class="btn btn-primary"/></td>
-                        <td></td>
-                    </tr>
-                </table>
-            </form>
-        </div>
-
-        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-        <script type="text/javascript">
-            $(document).ready(function() {
-                var rep = ['Oui', 'Eventuellement', 'Peu importe', 'Pas vraiment', 'Non'];
-                $.ajax({
-                    url: "/TenAsMarreDeTonWallpaper/api/categorie/getAll",
-                    type: "POST",
-                    success: function(data, textStatus, jqXHR) {
-                        var chaine = "";
-                        var res = JSON.parse(data);
-                        for(var i=0; i<res.data.length; i++) {
-                            chaine += "<input type='checkbox' value='" + res.data[i].id + "' name='categories[]' /> " + res.data[i].nom + "<br />";
-                        }
-                        $("#cat").html(chaine);
-
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var min = [0, 25, 50, 75];
+            var max = [24, 49, 74, 100];
+            $.ajax({
+                url: "/TenAsMarreDeTonWallpaper/api/categorie/getAll",
+                type: "POST",
+                success: function(data, textStatus, jqXHR) {
+                    var chaine = "";
+                    var res = JSON.parse(data);
+                    for(var i=0; i<res.data.length; i++) {
+                        chaine += "<input type='checkbox' value='" + res.data[i].id + "' name='categories[]' /> " + res.data[i].nom + "<br />";
                     }
-                });
+                    $("#cat").html(chaine);
 
-                $.ajax({
-                    url: "/TenAsMarreDeTonWallpaper/api/question/getAll",
-                    type: "POST",
-                    success: function(data, textStatus, jqXHR) {
-                        var chaine = "";
-                        var res = JSON.parse(data);
-                        for(var i=0; i<res.data.length; i++) {
-                            chaine += "<tr><td>" + res.data[i].q_courte + "</td><td><select name='rep[" + res.data[i].id + "]'>";
-                            for(var j=0; j<rep.length; j++) {
-			    	selected = (j==2 ? "selected" : "");
-                                chaine += "<option " + selected + " value='" + j + "'>" + rep[j] + "</option>";
-                            }
-                            chaine += "</select></td></tr>";
-                        }
-                        $("#rep").append(chaine);
-                    }
-                });
+                }
             });
 
-        </script>
-    </body>
+            $.ajax({
+                url: "/TenAsMarreDeTonWallpaper/api/question/getAll",
+                type: "POST",
+                success: function(data, textStatus, jqXHR) {
+                    var chaine = "";
+                    var res = JSON.parse(data);
+                    for(var i=0; i<res.data.length; i++) {
+                        chaine += "<tr><td>" + res.data[i].q_courte + "</td>";
+                        chaine += "<td><select name='rep[" + res.data[i].id + "][0]'>";
+                        for(var j=0; j<min.length; j++) {
+                            selected = (j==0 ? "selected" : "");
+                            chaine += "<option " + selected + " value='" + min[j] + "'>" + min[j] + "</option>";
+                        }
+                        chaine += "</select></td><td><select name='rep[" + res.data[i].id + "][1]'>";
+                        for(var j=0; j<max.length; j++) {
+                            selected = (j==1 ? "selected" : "");
+                            chaine += "<option " + selected + " value='" + max[j] + "'>" + max[j] + "</option>";
+                        }
+                        chaine += "</select></td></tr>";
+                    }
+                    $("#rep").append(chaine);
+                }
+            });
+        });
+
+    </script>
+</body>
 </html>
