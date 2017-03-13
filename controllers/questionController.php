4fe880b46b00d494ab662efe18bc13723ab15be6
Jordan Vilsaint <vilsaint.jordan@gmail.com>
2017-03-13 13:16:52 +0100

    correction questionController

--- controllers/questionController.php
+++ controllers/questionController.php
@@ -24,15 +24,16 @@ class questionController extends Controller {
 
     public function add() {
         $question = new Question();
-        if !(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+        $data = ['returnCode' => '', 'data' => '', 'returnMessage' => ''];
+        if (!(isset($_SESSION['user']) && !empty($_SESSION['user']))) {
         	$data = ['returnCode' => '0', 'data' => '', 'returnMessage' => 'Vous n\'etes pas connecté !'];	
         }
         else {
         	if (isset($_POST['q_courte']) && !empty($_POST['q_courte']) && isset($_POST['q_longue']) && !empty($_POST['q_longue']) && isset($_POST['importance']) && !empty($_POST['importance']) && isset($_POST['categories']) && !empty($_POST['categories'])) {
-        		$q_courte = $_POST['q_courte']);
-        		$q_longue = $_POST['q_longue']);
-        		$importance = $_POST['importance']);
-        		$categories = $_POST['categories']);
+        		$q_courte = $_POST['q_courte'];
+        		$q_longue = $_POST['q_longue'];
+        		$importance = $_POST['importance'];
+        		$categories = $_POST['categories'];
         		$idUser = $_SESSION['user']['id'];
         		$data = $question->add($q_courte, $q_longue, $importance, $idUser, $categories);	
         	}
@@ -46,7 +47,7 @@ class questionController extends Controller {
 
     public function delete() {
         $question = new Question();
-        if !(isset($_SESSION['user'] && ($_SESSION['user']['moderateur'] == 1 || $_SESSION['user']['admin'] == 1)) {
+        if (!(isset($_SESSION['user']) && ($_SESSION['user']['moderateur'] == 1 || $_SESSION['user']['admin'] == 1))) {
         	$data = ['returnCode' => '0', 'data' => '', 'returnMessage' => 'Vous n\'etes pas connecté !'];	
         }
         else {
