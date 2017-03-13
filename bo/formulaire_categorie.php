1128fb30094861574a7e762108fc601ef693039d
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-03-13 16:10:58 +0100

    Formulaire catégorie opérationnel

--- bo/formulaire_categorie.php
+++ bo/formulaire_categorie.php
@@ -3,75 +3,80 @@ $page['title'] = "Catégorie";
 include('header.php');
 ?>
 
-        <!--<h1>Formulaire catégorie</h1>-->
-        <h4>Ajouter une nouvelle catégorie</h4>
-        <hr>
-
-        <form action="../api/categorie/add" method="post">
-            <table>
+<div class="col-md-12">
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>Nom<span style="color:red;">*</span> :</td>
-                    <td><input type="text" name="nom" class="form-control" /></td>
+                    <th>#</th>
+                    <th>Nom</th>
                 </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="submit" value="Ajouter" name="submit" class="btn btn-primary"/></td>
-                </tr>
-            </table>
-        </form>
-        <?php
-            }
-            if(isset($_POST['submit']))
-            {
-                if(isset($_POST['nom']) && !empty($_POST['nom']))
-                {
-                    $_SESSION['nom'] = $_POST['nom'];
-                    
-                    $c->add($_SESSION['nom']);
-                    echo "<h3>Bravo ta catégorie a bien été rajoutée !</h3>";
-                }
-                else
-                {
-                    echo "T'as dû faire une erreur !";
-                }
-            }
-            ?>
+            </thead>
+            <tbody id="req1">
+            </tbody>
+        </table>
     </div>
-    <fieldset>
-            <legend>DELETE CATEGORIE</legend>
-            <form action="../api/categorie/delete" method="post">
-                <table>
-                    <tr>
-                        <td> <label for="new_cat">Nom de la catégorie :</label></td>
-                        <select name="categorie_id">
-                            <?php
-                            $allCategories = $c->getAll();
-                            
-                            foreach ($allCategories as $cat):
-                            ?>
-                            <option value="<?= $cat['id'] ?>"><?php echo $cat['nom'] ?></option>
-                            <?php
-                                endforeach;
-                            ?>
-                        </select>
-                    </tr>
-                    <tr>
-                        <td><input type="submit" value="Supprimer la catégorie" name="deleteCat" /></td>
-                    </tr>
-                </table>
-            </form>
-            <?php
-                if(isset($_POST['deleteCat']))
-                {
-                    $c->delete($cat['id']);
-                    echo "<h3>Bravo ta catégorie a bien été supprimée !</h3>";
+</div>
+
+<div class="col-md-6 col-sm-12">
+    <h2 class="sub-header">Ajouter une catégorie</h2>
+    <form action="../api/categorie/add" method="POST" class="form-inline">
+        <div class="form-group">
+            <label for="nom">Nom<span style="color:red;">*</span> :</label>
+            <input type="text" class="form-control" name="nom" id="nom" />
+        </div>
+        <input type="submit" value="Ajouter" name="submit" class="btn btn-success" />
+    </form>
+</div>
+
+<div class="col-md-6 col-sm-12">
+    <h2 class="sub-header">Modifier une catégorie</h2>
+    <form action="../api/categorie/change" method="POST" class="form-inline">
+        <div class="form-group">
+           <select name="id" class="select form-control"></select>
+            <label for="nom">Nouveau nom<span style="color:red;">*</span> :</label>
+            <input type="text" class="form-control" name="nom" id="nom" />
+        </div>
+        <input type="submit" value="Modifier" name="submit" class="btn btn-info" />
+    </form>
+</div>
+
+<div class="col-md-6 col-sm-12">
+    <h2 class="sub-header">Supprimer une catégorie</h2>
+    <form action="../api/categorie/delete" method="POST" class="form-inline">
+        <select name="id" class="select form-control"></select>
+        <input type="submit" value="Supprimer" name="submit" class="btn btn-danger" />
+    </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $.ajax({
+            url: "/TenAsMarreDeTonWallpaper/api/categorie/getAll",
+            type: "POST",
+            success: function(data, textStatus, jqXHR) {
+                var chaine = "";
+                var res = JSON.parse(data);
+                for(var i=0; i<res.data.length; i++) {
+                    chaine += '<tr><td>' + res.data[i].id + '</td>';
+                    chaine += '<td>' + res.data[i].nom + '</td></tr>';
                 }
-            ?>
-        </fieldset>
-</body>
-</html>
+                $("#req1").html(chaine);
+                
+                chaine = "";
+                
+                for(var i=0; i<res.data.length; i++) {
+                    chaine += "<option value='" + res.data[i].id + "'>" + res.data[i].nom + "</option>";
+                }
+                
+                $(".select").html(chaine);
+            }
+        });
+    });
+
+</script>
 
 <?php
 include('footer.php');
 ?>
-
