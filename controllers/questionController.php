1c9b9ad79498c5349352dc693c7a8b2642e42d18
Jordan Vilsaint <vilsaint.jordan@gmail.com>
2017-03-13 11:55:49 +0100

    questionController OK

--- controllers/questionController.php
+++ controllers/questionController.php
@@ -15,4 +15,50 @@ class questionController extends Controller {
         $data = $question->getAll();
         echo json_encode($data);
     }
+
+    public function get($id) {
+        $question = new Question();
+        $data = $question->get($id);
+        echo json_encode($data);
+    }
+
+    public function add() {
+        $question = new Question();
+        if !(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+        	$data = ['returnCode' => '0', 'data' => '', 'returnMessage' => 'Vous n\'etes pas connecté !'];	
+        }
+        else {
+        	if (isset($_POST['q_courte']) && !empty($_POST['q_courte']) && isset($_POST['q_longue']) && !empty($_POST['q_longue']) && isset($_POST['importance']) && !empty($_POST['importance']) && isset($_POST['categories']) && !empty($_POST['categories'])) {
+        		$q_courte = $_POST['q_courte']);
+        		$q_longue = $_POST['q_longue']);
+        		$importance = $_POST['importance']);
+        		$categories = $_POST['categories']);
+        		$idUser = $_SESSION['user']['id'];
+        		$data = $question->add($q_courte, $q_longue, $importance, $idUser, $categories);	
+        	}
+			else {
+				$data = ['returnCode' => '-2', 'data' => '', 'returnMessage' => 'Certains paramètres sont manquants, veuillez vérifier'];
+			}
+        }
+
+        echo json_encode($data);
+    }
+
+    public function delete() {
+        $question = new Question();
+        if !(isset($_SESSION['user'] && ($_SESSION['user']['moderateur'] == 1 || $_SESSION['user']['admin'] == 1)) {
+        	$data = ['returnCode' => '0', 'data' => '', 'returnMessage' => 'Vous n\'etes pas connecté !'];	
+        }
+        else {
+        	if (isset($_POST['id']) && !empty($_POST['id'])) {
+        		$data = $question->delete($_POST['id']);
+        	}
+			else {
+				$data = ['returnCode' => '-2', 'data' => '', 'returnMessage' => 'Certains paramètres sont manquants, veuillez vérifier'];
+			}
+
+        }
+        echo json_encode($data);
+
+    }
 }
